af8ec92543c1bb8a6064eb9f9057c2914dae9668
Pablo Marti <pablo@moonfarmer.com>
2018-06-19 17:30:15 -0400

    update script to do the following; create a curl request; source the dot env file; make a curl call to post to the webhook

--- iwmd.sh
+++ iwmd.sh
@@ -1,1 +1,20 @@
 #!/usr/bin/env bash
+
+create_curl_request() {
+  json_payload="{
+  \"attachments\" : [
+  {
+    \"text\" : \"$IWMD\",
+    \"color\": \"#D2B48C\",
+    \"unfurl_links\": \"true\"
+  }
+  ]
+}"
+}
+
+# grab .env
+PWD=$(pwd)
+source $PWD/.env
+create_curl_request
+
+curl -X POST --data-urlencode "payload=$json_payload" $WEBHOOK
